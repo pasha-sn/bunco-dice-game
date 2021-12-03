ba0654f14df05f8d81bce35fae0da15374d21047
Pasha Sadi <pasha.sn@gmail.com>
2021-12-02 20:47:42 -0500

    2021-12-02-8-47-pm

--- script.js
+++ script.js
@@ -2,17 +2,44 @@
 
 const btnRoll = document.querySelector(".btn-roll");
 const btnStart = document.querySelector(".btn-start");
-const player1 = document.querySelector(".player-1");
-const player2 = document.querySelector(".player-2");
+
+const labelName1 = document.querySelector(".player-1");
+const labelName2 = document.querySelector(".player-2");
+
 const input1 = document.querySelector(".input1");
 const input2 = document.querySelector(".input2");
+
 const current1 = document.querySelector(".current-1");
 const current2 = document.querySelector(".current-2");
+
 const total1 = document.querySelector(".total-1");
 const total2 = document.querySelector(".total-2");
+
 const cart1 = document.querySelector(".cart-1");
 const cart2 = document.querySelector(".cart-2");
+
 const diceArea = document.querySelector(".dice-area");
+
+const player1 = {
+  part: 1,
+  name: "player1",
+  currentScore: 0,
+  roundScore: 0,
+  round: 1,
+  active: true,
+  scores: [],
+};
+
+const player2 = {
+  part: 2,
+  name: "player2",
+  currentScore: 0,
+  roundScore: 0,
+  round: 1,
+  active: false,
+  scores: [],
+};
+
 //function
 
 //1- Roll Dice
@@ -51,12 +78,16 @@ const currentScore = function (round, dice) {
 //displays
 const switchPlayer = function () {
   console.log("%cSwitching Player", "color: green; font-size: 1rem");
-  if ([...cart1.classList].includes("border-dark")) {
+  if (player1.active) {
     cart1.classList.remove("border-dark");
     cart2.classList.add("border-dark");
+    player1.active = false;
+    player2.active = true;
   } else {
     cart2.classList.remove("border-dark");
     cart1.classList.add("border-dark");
+    player2.active = false;
+    player1.active = true;
   }
 };
 const displayDice = function (dice) {
@@ -74,16 +105,19 @@ const displayCurrentScore = function (score) {
 
 btnStart.addEventListener("click", function (e) {
   e.preventDefault();
-  // console.log("in event handler: ", input1.value);
+
+  player1.name = labelName1.textContent = input1.value;
+  player2.name = labelName2.textContent = input2.value;
+
   cart1.classList.add("border-dark");
   cart2.classList.remove("border-dark");
 
-  player1.textContent = input1.value;
-  player2.textContent = input2.value;
-  current1.textContent = 0;
-  current2.textContent = 0;
+  current1.textContent = "Current";
+  current2.textContent = "Current";
   total1.textContent = 0;
   total2.textContent = 0;
+  diceArea.textContent = `Round 1, ${player1.name}
+   please Roll Dice`;
 });
 
 btnRoll.addEventListener("click", function () {
@@ -91,20 +125,24 @@ btnRoll.addEventListener("click", function () {
   let dice;
   let current;
   let total;
-  const isPlayer1 = [...cart1.classList].includes("border-dark");
-  console.log("btnDice listener", isPlayer1);
-  if (isPlayer1) {
+  let activePlayer;
+  player1.active ? (activePlayer = player1) : (activePlayer = player2);
+
+  diceArea.textContent = dice = rollDice();
+
+  //calculate current score
+  current = currentScore(activePlayer.round, dice);
+
+  if (activePlayer.part === 1) {
     total = Number(total1.textContent);
-    diceArea.textContent = dice = rollDice();
-    current1.textContent = current = currentScore(1, dice);
+    current1.textContent = current;
   } else {
     total = Number(total2.textContent);
-    diceArea.textContent = dice = rollDice();
-    current2.textContent = current = currentScore(1, dice);
+    current2.textContent = current;
   }
 
   if (current > 0) {
-    if (isPlayer1) {
+    if (activePlayer.part === 1) {
       total += current;
       total1.textContent = total;
     } else {
@@ -114,57 +152,4 @@ btnRoll.addEventListener("click", function () {
   } else {
     switchPlayer();
   }
-
-  //   let totalSet1 = 0;
-  //   let dice = rollDice();
-  //   console.log(dice);
-  //   displayDice(dice);
-  //   const current = currentScore(1, dice);
-  //   displayCurrentScore(current);
-  //   if (current > 0) {
-  //     totalSet1 += current;
-  //     totalScore1 += totalSet1;
-  //     displayTotalScore(totalScore1);
-  //     console.log("Roll Again!");
 });
-
-// Arash
-// //displays
-// const switchPlayer = function () {
-//   console.log("%cSwitching Player", "color: green; font-size: 1rem");
-// };
-
-// const displayDice = function (dice) {
-//   document.querySelector(".dice-show").textContent = [...dice];
-// };
-
-// const displayTotalScore = function (score) {
-//   document.querySelector(".total-score-1").textContent = score;
-
-//   console.log("Total: ", score);
-// };
-
-// const displayCurrentScore = function (score) {
-//   document.querySelector(".current-score-1").textContent = score;
-//   console.log("Current: ", score);
-// };
-
-// let totalScore1 = 0;
-// let totalScore2 = 0;
-
-// document.querySelector(".btn-dice").addEventListener("click", function () {
-//   let totalSet1 = 0;
-//   let dice = rollDice();
-//   console.log(dice);
-//   displayDice(dice);
-//   const current = currentScore(1, dice);
-//   displayCurrentScore(current);
-//   if (current > 0) {
-//     totalSet1 += current;
-//     totalScore1 += totalSet1;
-//     displayTotalScore(totalScore1);
-//     console.log("Roll Again!");
-
-//     // document.querySelector(".btn-dice").addEventListener('click', )
-//   } else switchPlayer();
-// });
